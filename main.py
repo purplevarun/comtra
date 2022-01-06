a1575508874b615d90e49bea78336e223b64d93f
purplevarun <purplevarun@gmail.com>
2022-01-06 13:01:14 +0530

    some error with pygame.sprite

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+# import python modules
 import pygame
 
-pygame.init()
+# import local modules
+import src.classes
 
+# initialization
+pygame.init()
 screenWidth = 800
 screenHeight = int(0.8 * screenWidth)
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+pygame.display.set_caption("Comtra")
+
+
+# game loop
+gameRunning = True
+while gameRunning:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameRunning = False
---
+++ src/classes.py
@@ -0,0 +1,6 @@
+import pygame
+
+
+def Player(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale):
+        pygame.sprite.Sprite.__init__(self)
